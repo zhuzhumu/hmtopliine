81722925504c753442168547b744ba4571d59584
zhuzhumu <1370623302@qq.com>
2020-03-31 11:00:34 +0800

    '初始化项目'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Storte from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  Storte,
   router,
   render: h => h(App)
 }).$mount('#app')
